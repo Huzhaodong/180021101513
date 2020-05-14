2d97ef95fae04b63939a881be05713d9da395211
Huzhaodong1 <390729204@qq.com>
2020-05-14 13:18:27 +0800

    new file hello.py

---
+++ hello.py
@@ -0,0 +1,37 @@
+import turtle
+def drawGap():
+    turtle.penup()
+    turtle.fd(5)
+def drawLine(draw):
+    drawGap()
+    turtle.pendown() if draw else turtle.penup()
+    turtle.fd(40)
+    drawGap()
+    turtle.right(90)
+def drawDigit(digit):
+    drawLine(True) if digit in [2, 3, 4, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 1, 3, 4, 5, 6, 7, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 2, 3, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 2, 6, 8] else drawLine(False)
+    turtle.left(90)
+    drawLine(True) if digit in [0, 4, 5, 6, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 2, 3, 5, 6, 7, 8, 9] else drawLine(False)
+    drawLine(True) if digit in [0, 1, 2, 3, 4, 7, 8, 9] else drawLine(False)
+    turtle.left(180)
+    turtle.penup()
+    turtle.fd(20)
+def drawDate(date):
+    for i in date:
+        if i == '-':
+            turtle.pencolor("red")
+            turtle.write('计时结束', font=("Arial", 18, "normal"))
+        else:
+            turtle.pencolor("blue")
+            drawDigit(eval(i))
+            turtle.clearscreen()
+def main():
+    turtle.setup(800, 350, 200, 200)
+    drawDate('9876543210-')
+    turtle.hideturtle()
+    turtle.done()
+main()
